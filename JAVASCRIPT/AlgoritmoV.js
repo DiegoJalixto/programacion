58a2c15bdc04836c3422cc0397a0b0ece9f213c8
DiegoJalixto <djalixtoc@gmail.com>
2023-09-27 14:15:53 -0500

    Boostrap

---
+++ JAVASCRIPT/AlgoritmoV.js
@@ -0,0 +1,14 @@
+var arr = [1, 2, 3, 4, 5];
+var sum = 0;
+
+for(var i = 0 ; i<arr.length;i++){
+
+    sum = sum + arr[i];
+    promedio = sum / arr.length
+
+    console.log(promedio);
+}
+
+
+// console.log(Math.max(...arr));
+// console.log(Math.min(...arr));
